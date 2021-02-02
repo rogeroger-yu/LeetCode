d792cf0bfd7569167372d6a1dff9feb5ff22da27
rogeryu <yu.qian@99cloud.net>
2021-02-02 23:25:16 +0800

    2. select sort

---
+++ 2.select_sort.py
@@ -0,0 +1,12 @@
+class Solution:
+    def select_sort(self, nums):
+        if not nums:
+            return
+        size = len(nums)
+        for i in range(size):
+            min_index = i
+            for j in range(i+1, size):
+                if nums[min_index] > nums[j]:
+                    min_index = j
+            nums[i], nums[min_index] = nums[min_index], nums[i]
+        return nums
